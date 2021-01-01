b57ed3fffe187353fbde687de947947de31e7f51
xwoud <xwoud@naver.com>
2021-01-02 04:05:43 +0900

    [upload] UIFont+Extension 파일 생성
    body: Font의 종류가 많기 때문에 size를 따로 지정해서 할 수 있게 제작

---
+++ meaning/meaning/Global/Extension/UIFont+Extension.swift
@@ -0,0 +1,59 @@
+//
+//  UIFont+Extension.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/02.
+//
+
+import UIKit
+
+extension UIFont {
+    // MARK: SpoqaHanSansNeo Font
+    class func spoqaThin(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Thin", size: size)!
+    }
+    
+    class func spoqaLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Light", size: size)!
+    }
+    
+    class func spoqaRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Regular", size: size)!
+    }
+    
+    class func spoqaMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Medium", size: size)!
+    }
+    
+    class func spoqaBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Bold", size: size)!
+    }
+    
+    // MARK: NanumBarunpen Font
+    class func nanumRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "NanumBarunpen", size: size)!
+    }
+    
+    // MARK: NotoSansCJKKR Font
+    class func notoThin(size: CGFloat) -> UIFont {
+        return UIFont(name: "NotoSansCJKKR-Thin", size: size)!
+    }
+    
+    class func notoLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "NotoSansCJKKR-Light", size: size)!
+    }
+    
+    class func notoRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: size)!
+    }
+    
+    class func notoMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "NotoSansCJKKR-Medium", size: size)!
+    }
+    
+    class func notoBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: size)!
+    }
+    
+}
+
