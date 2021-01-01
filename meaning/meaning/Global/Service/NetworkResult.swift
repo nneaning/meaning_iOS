8cb5f2a96778ca10abddac7e25016f7fbda08aba
xwoud <xwoud@naver.com>
2021-01-02 00:18:58 +0900

    [upload] NetworkResult 파일 생성
    body: 서버 통신 시 네트워크 상태를 받아오는 구조체 생성

---
+++ meaning/meaning/Global/Service/NetworkResult.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkResult.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/02.
+//
+
+import Foundation
+
+enum NetworkResult<T> {
+    case success(T)
+    case requestErr(T)
+    case pathErr
+    case serverErr
+    case networkFail
+}
