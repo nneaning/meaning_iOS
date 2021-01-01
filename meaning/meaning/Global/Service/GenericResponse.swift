2d6b0f6f0dd897d661da45be9aa75b3ec940811f
xwoud <xwoud@naver.com>
2021-01-02 00:20:18 +0900

    [upload] GenericResponse 파일 생성
    body: 서버에서 값을 받아올 때 status,success,message,data를 받아올 수 있는 구조체 모델 생성

---
+++ meaning/meaning/Global/Service/GenericResponse.swift
@@ -0,0 +1,32 @@
+//
+//  GenericResponse.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/02.
+//
+
+import Foundation
+
+struct GenericResponse<T: Codable>: Codable {
+    
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: T?
+    
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case success = "success"
+        case message = "message"
+        case data = "data"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(Int.self, forKey: .status)) ?? -1
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode(T.self, forKey: .data)) ?? nil
+    }
+    
+}
