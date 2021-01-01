22fe961d6154cfaf1acad1af23547702901cfaa1
xwoud <xwoud@naver.com>
2021-01-01 22:22:01 +0900

    [upload] xcode project 생성
    body: 프로젝트명 - meaning
    ios 최소 버전 - 13.0

---
+++ meaning/meaning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
