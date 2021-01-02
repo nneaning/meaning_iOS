8878660a2100aee9c2f18cd14e55d674800e362a
xwoud <xwoud@naver.com>
2021-01-02 23:57:45 +0900

    [styel] 카메라 촬영 뷰 - IBOutlet 잡기

--- meaning/meaning/Screen/Camera/ViewController/TimeStampVC.swift
+++ meaning/meaning/Screen/Camera/ViewController/TimeStampVC.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class TimeStampVC: UIViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var stampBorderView: UIView!
+    @IBOutlet weak var stampTimeLabel: UILabel!
+    @IBOutlet weak var stampDateLabel: UILabel!
+    @IBOutlet weak var stampLogoImage: UIImageView!
+    
+    @IBOutlet weak var cancleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
