9053c6970dee21d7544ef47f62272b2ba55dc5ff
xwoud <xwoud@naver.com>
2021-01-03 01:18:18 +0900

    [upload] Font 추가
    body: NanumSquareOTFR
    NanumSquareOTFL
    NanumSquareOTFB
    NanumSquareOTFEB

--- meaning/meaning/Global/Extension/String+Extension.swift
+++ meaning/meaning/Global/Extension/String+Extension.swift
@@ -23,6 +23,21 @@ extension String {
         
         return formatter.string(from: tempDate)
     }
+    func recordTime() -> String {
+        // "yyyy-MM-dd HH:mm:ss"형식의 string 시간을 "yyyy년 MM월 dd일" 형식의 string time으로 바꾸기
+        var format = "yyyy-MM-dd HH:mm:ss"
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        formatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+        guard let tempDate = formatter.date(from: self) else {
+            return ""
+        }
+        
+        format = "HH:mm"
+        formatter.dateFormat = format
+        
+        return formatter.string(from: tempDate)
+    }
     
     func stringToDate() -> Date {
         // string타입을 date타입으로 바꾸기
