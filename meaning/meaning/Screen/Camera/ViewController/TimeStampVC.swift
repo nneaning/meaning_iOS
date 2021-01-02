d387b1a5304f2a4f317dbc28f228bd9133028b96
xwoud <xwoud@naver.com>
2021-01-03 00:49:28 +0900

    [style] 카메라 촬영 뷰 - Font 및 디자인
    body: TimeStamp가 찍히는 뷰를 기준으로 디자인 완성
    취소 버튼은 버튼 Label 크기보다 터치 영역 증가
    
    Related to: #5

--- meaning/meaning/Screen/Camera/ViewController/TimeStampVC.swift
+++ meaning/meaning/Screen/Camera/ViewController/TimeStampVC.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TimeStampVC: UIViewController {
 
-    // MARK: - IBOutlet
+    // MARK: IBOutlet
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var stampBorderView: UIView!
     @IBOutlet weak var stampTimeLabel: UILabel!
@@ -18,10 +18,24 @@ class TimeStampVC: UIViewController {
     
     @IBOutlet weak var cancleButton: UIButton!
     
+    // MARK: Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setCameraView()
     }
 
+}
+extension TimeStampVC {
+    // MARK: Function
+    func setCameraView() {
+        // 카메라 뷰 Style Setting
+        stampBorderView.setBorder(borderColor: .white, borderWidth: 1)
+        stampBorderView.backgroundColor = .none
+        
+        stampLogoImage.image = UIImage(named: "cameraTimestampLogo")
+        
+        cancleButton.setTitle("취소", for: .normal)
+        cancleButton.titleLabel?.font = UIFont.spoqaRegular(size: 17)
+        cancleButton.tintColor = .white
+    }
 }
