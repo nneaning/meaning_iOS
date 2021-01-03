8d3a42ccce84ebbd80728e187fe4cbb7437e4897
xwoud <xwoud@naver.com>
2021-01-03 19:01:28 +0900

    [style] PhotoCheckVC Font 및 텍스트 추가
    body: TimeStampVC 와 Style 동일
    
    Related to: #15

--- meaning/meaning/Screen/Camera/ViewController/PhotoCheckVC.swift
+++ meaning/meaning/Screen/Camera/ViewController/PhotoCheckVC.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class PhotoCheckVC: UIViewController {
+    
+    // MARK: Variable Part
+    
+    var photoImage: UIImage?
+    var photoTime: String = "15:00"
+    var photoDate: String = "2021년 01월 03일 (일)"
+    // TimeStampVC 에서 받아와야 하는 데이터들(사진,시간)
 
     // MARK: IBOutlet
     
     @IBOutlet weak var frameView: UIView!
     @IBOutlet weak var stampImage: UIImageView!
     @IBOutlet weak var stampTimeLabel: UILabel!
+    @IBOutlet weak var stampBorderView: UIView!
     @IBOutlet weak var stampDateLabel: UILabel!
     @IBOutlet weak var stampLogoImage: UIImageView!
     
@@ -24,6 +32,8 @@ class PhotoCheckVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCameraView()
+        setButton()
     }
 
 }
@@ -33,5 +43,36 @@ class PhotoCheckVC: UIViewController {
 extension PhotoCheckVC {
     
     // MARK: Function
+    
+    func setCameraView() {
+        // 카메라 뷰 Style Setting
+        stampBorderView.setBorder(borderColor: .white, borderWidth: 1)
+        stampBorderView.backgroundColor = .none
+        
+        stampTimeLabel.font = UIFont.nsLight(size: 70)
+        stampTimeLabel.textColor = .white
+        stampDateLabel.font = UIFont.spoqaRegular(size: 16)
+        stampDateLabel.textColor = .white
+        
+        stampTimeLabel.text = photoTime
+        stampDateLabel.text = photoDate
+        
+        if let photoImage = photoImage {
+            stampImage.image = photoImage
+        }
+        
+        stampLogoImage.image = UIImage(named: "cameraTimestampLogo")
+    }
+    
+    func setButton() {
+        // 버튼 Style Setting
+        retakeButton.setTitle("다시 찍기", for: .normal)
+        retakeButton.titleLabel?.font = UIFont.spoqaRegular(size: 17)
+        retakeButton.tintColor = .white
+        
+        useButton.setTitle("사진 사용", for: .normal)
+        useButton.titleLabel?.font = UIFont.spoqaRegular(size: 17)
+        useButton.tintColor = .white
+    }
     
 }
