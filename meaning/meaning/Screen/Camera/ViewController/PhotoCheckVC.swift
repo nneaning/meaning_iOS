28807eeb419e1e94e6d656fa27bef331731a4b36
xwoud <xwoud@naver.com>
2021-01-03 18:47:56 +0900

    [feat] IBOutlet 추가

--- meaning/meaning/Screen/Camera/ViewController/PhotoCheckVC.swift
+++ meaning/meaning/Screen/Camera/ViewController/PhotoCheckVC.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class PhotoCheckVC: UIViewController {
 
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var frameView: UIView!
+    @IBOutlet weak var stampImage: UIImageView!
+    @IBOutlet weak var stampTimeLabel: UILabel!
+    @IBOutlet weak var stampDateLabel: UILabel!
+    @IBOutlet weak var stampLogoImage: UIImageView!
+    
+    @IBOutlet weak var retakeButton: UIButton!
+    @IBOutlet weak var useButton: UIButton!
+    
+    // MARK: Life Cycle Part
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+
+}
+
+// MARK: Extension
+
+extension PhotoCheckVC {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // MARK: Function
+    
 }
