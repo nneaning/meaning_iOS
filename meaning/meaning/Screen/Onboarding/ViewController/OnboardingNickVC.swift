0ebf0c4373d3b501648cbacedbe83b0fdc0e72eb
xwoud <xwoud@naver.com>
2021-01-04 17:38:48 +0900

    [style] 온보딩 닉네임 치는 뷰 style setting
    body: 라벨에서 닉네임만 굵게 표시
    
    Related to: #24

--- meaning/meaning/Screen/Onboarding/ViewController/OnboardingNickVC.swift
+++ meaning/meaning/Screen/Onboarding/ViewController/OnboardingNickVC.swift
@@ -9,21 +9,54 @@ import UIKit
 
 class OnboardingNickVC: UIViewController {
 
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var explainLabel: UILabel!
+    @IBOutlet weak var nickTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    // MARK: Life Cycle Part
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
 
-        // Do any additional setup after loading the view.
     }
+
+}
+
+// MARK: Extension
+
+extension OnboardingNickVC {
     
+    // MARK: Function
+    
+    func setView() {
+        // 뷰 Style Setting
+        explainLabel.text = "사용하실 닉네임을 적어주세요."
+        explainLabel.font = UIFont.spoqaRegular(size: 22)
+        explainLabel.textColor = UIColor.meaningWhite
 
-    /*
-    // MARK: - Navigation
+        if let text = explainLabel.text {
+            // "닉네임" 부분에만 폰트를 다르게 설정
+            let attributedStr = NSMutableAttributedString(string: explainLabel.text ?? "")
+            attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String), value: UIFont.spoqaBold(size: 22), range: (text as NSString).range(of: "닉네임"))
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            explainLabel.attributedText = attributedStr
+        }
+        
+        nickTextField.font = UIFont.spoqaRegular(size: 22)
+        nickTextField.textColor = .meaningWhite
+        
+        nextButton.backgroundColor = .meaningIvory
+        nextButton.titleLabel?.font = UIFont.spoqaMedium(size: 16)
+        nextButton.setTitle("다음으로 넘어가기", for: .normal)
+        nextButton.makeRounded(cornerRadius: 6)
+        
+        nextButton.setTitleColor(.gray4, for: .normal)
+        nextButton.isEnabled = false
+        // 닉네임이 설정 안되었을 때는 넘어가기 불가
+       
+        
     }
-    */
-
 }
