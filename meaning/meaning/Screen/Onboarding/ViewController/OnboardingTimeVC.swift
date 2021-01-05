390e99670a30e8eecc70f1a0ab1b1827ac79c5e1
xwoud <xwoud@naver.com>
2021-01-05 20:49:06 +0900

    [feat] 뷰 클릭시 toolbar 내리기
    body: keyboard를 내리는 것과 같은 방식으로 접근
    
    Related to: #41

--- meaning/meaning/Screen/Onboarding/ViewController/OnboardingTimeVC.swift
+++ meaning/meaning/Screen/Onboarding/ViewController/OnboardingTimeVC.swift
@@ -11,8 +11,8 @@ class OnboardingTimeVC: UIViewController {
 
     // MARK: Variable Part
     
-    var userNick: String = "(이름)"
-    var wakeupTime: String = "오전 5시"
+    var userNick: String = "이름"
+    var wakeupTime: String?
     var hours: [Int] = []
     var minutes: [String] = []
     var userHours: Int = 5
@@ -42,7 +42,7 @@ extension OnboardingTimeVC {
     
     func setView() {
         // 뷰 Style Setting
-        explainLabel.text = "\(userNick) 님의\n목표 기상시간을 알려주세요.\n하루를 길게 쓸 수 있게\n도와 드릴게요!"
+        explainLabel.text = "\(userNick)님의\n목표 기상시간을 알려주세요.\n하루를 길게 쓸 수 있게\n도와 드릴게요!"
         explainLabel.numberOfLines = 0
         explainLabel.font = UIFont.spoqaLight(size: 22)
         explainLabel.textColor = .meaningWhite
@@ -71,20 +71,22 @@ extension OnboardingTimeVC {
     
     func setTimeTextField() {
         // timeTextField Sytle Setting
-        if let rightMinutes = userMinues {
-            wakeupTime = "오전 \(userHours)시 \(rightMinutes)분"
+        if let userMinues = userMinues {
+            wakeupTime = "오전 \(userHours)시 \(userMinues)분"
         } else {
             wakeupTime = "오전 \(userHours)시"
         }
         
-        timeTextField.text = "\(wakeupTime)에 일어날래요."
-        if let text = timeTextField.text {
-            // 시간에만 밑줄과 폰트 굵게 설정
-            let attributedStr = NSMutableAttributedString(string: text)
-            attributedStr.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.thick.rawValue,  range: (text as NSString).range(of: wakeupTime))
-            attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String), value: UIFont.spoqaMedium(size: 28), range: (text as NSString).range(of: wakeupTime))
-            
-            timeTextField.attributedText = attributedStr
+        if let wakeupTime = wakeupTime {
+            timeTextField.text = "\(wakeupTime)에 일어날래요."
+            if let text = timeTextField.text {
+                // 시간에만 밑줄과 폰트 굵게 설정
+                let attributedStr = NSMutableAttributedString(string: text)
+                attributedStr.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.thick.rawValue,  range: (text as NSString).range(of: wakeupTime))
+                attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String), value: UIFont.spoqaMedium(size: 28), range: (text as NSString).range(of: wakeupTime))
+                
+                timeTextField.attributedText = attributedStr
+            }
         }
     }
     
@@ -110,6 +112,12 @@ extension OnboardingTimeVC {
         timeTextField.inputView = pickerView
         pickerView.selectRow(1, inComponent: 0, animated: false)
         // pickerView 기본값은 오전 5시 00분
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 뷰 클릭 시 toolbar 내리기
+        setTimeTextField()
+        view.endEditing(true)
     }
 
 }
