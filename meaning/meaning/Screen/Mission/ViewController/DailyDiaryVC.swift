94f988e813599139ddc4adacd825b68117872272
MinseungSeon <seonminseung@naver.com>
2021-01-05 20:48:41 +0900

    [layout] 짧은 독서 뷰 레이아웃 잡음

--- meaning/meaning/Screen/Mission/ViewController/DailyDiaryVC.swift
+++ meaning/meaning/Screen/Mission/ViewController/DailyDiaryVC.swift
@@ -55,7 +55,8 @@ class DailyDiaryVC: UIViewController {
         self.bodyUpperLabel.font = UIFont.notoRegular(size: 18.0)
         self.bodyUpperLabel.textColor = UIColor.gray2
         self.bodyUpperLabel.text = "오늘 아침을 글로 남겨봐요"
-        
+        self.bodyUpperLabel.lineSetting(kernValue: -0.72)
+
         self.bodyView.setRounded(radius: 6)
         self.bodyView.backgroundColor = UIColor.meaningIvory
         self.bodyTextView.backgroundColor = UIColor.white.withAlphaComponent(0)
@@ -115,39 +116,3 @@ extension DailyDiaryVC: UITextViewDelegate {
         return true
     }
 }
-
-// MARK: Extension
-
-extension UIViewController {
-    
-    // MARK: Function
-    
-    func showToast(message : String, font: UIFont) {
-        let guide = view.safeAreaInsets.bottom
-        let height = self.view.frame.size.height-guide
-        
-        let toastLabel = UILabel(
-            frame: CGRect( x: self.view.frame.size.width/2 - 83,
-                           y: height-181,
-                           width: 166,
-                           height: 29
-            )
-        )
-        
-        toastLabel.backgroundColor = UIColor.gray4
-        toastLabel.textColor = UIColor.gray6
-        toastLabel.font = font
-        toastLabel.textAlignment = .center
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 6
-        toastLabel.clipsToBounds  =  true
-        self.view.addSubview(toastLabel)
-        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
-        })
-    }
-}
-
