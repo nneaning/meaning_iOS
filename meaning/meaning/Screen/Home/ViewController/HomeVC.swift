a1c2f0dc7288b7644c1c3a193a723a6afb2d95ee
xwoud <xwoud@naver.com>
2021-01-06 18:44:43 +0900

    [style] homeVC view style setting
    body: 멘트 라벨 수정
    
    Related to: #48

--- meaning/meaning/Screen/Home/ViewController/HomeVC.swift
+++ meaning/meaning/Screen/Home/ViewController/HomeVC.swift
@@ -21,6 +21,7 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
         setList()
     }
 
@@ -31,6 +32,34 @@ class HomeVC: UIViewController {
 extension HomeVC {
     
     // MARK: Function
+    
+    func setView() {
+        
+        self.view.backgroundColor = .meaningIvory
+        
+        explainLabel.text = "좋은 아침입니다!\n순서대로 기상미션을\n수행해볼까요?"
+        explainLabel.textColor = .meaningNavy
+        explainLabel.font = UIFont.spoqaLight(size: 22)
+        explainLabel.numberOfLines = 0
+        
+        if let text = explainLabel.text {
+            // "좋은 아침입니다!" 부분에만 폰트를 다르게 설정
+            let attributedStr = NSMutableAttributedString(string: explainLabel.text ?? "")
+            attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String), value: UIFont.spoqaMedium(size: 22), range: (text as NSString).range(of: "좋은 아침입니다!"))
+
+            explainLabel.attributedText = attributedStr
+        }
+        
+        dateButton.isEnabled = false
+        dateButton.makeRounded(cornerRadius: 15)
+        dateButton.layer.borderColor = UIColor.meaningNavy.cgColor
+        dateButton.layer.borderWidth = 1
+        dateButton.setTitle(Date().datePickerToString().recordDate(), for: .normal)
+        dateButton.titleLabel?.font = UIFont.spoqaRegular(size: 14)
+        dateButton.titleLabel?.lineSetting(kernValue: -0.88, lineSpacing: 20)
+        dateButton.setTitleColor(.meaningNavy, for: .normal)
+        
+    }
     
     func setList() {
         let mission1 = MissionCard(index: 1, ment: "타임카메라로\n미라클 모닝 인증하기", imageName: "homeCard1")
