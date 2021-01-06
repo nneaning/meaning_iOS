ad3067c7537d37f3a337a7bdde45c52796e224a8
xwoud <xwoud@naver.com>
2021-01-06 18:31:38 +0900

    [style] cell style 적용
    body: 멘트, 인덱스 폰트 크기,색깔 지정
    
    Related to: #48

--- meaning/meaning/Screen/Home/Cell/MissionCardCell.swift
+++ meaning/meaning/Screen/Home/Cell/MissionCardCell.swift
@@ -9,4 +9,31 @@ import UIKit
 
 class MissionCardCell: UICollectionViewCell {
     static let identifier: String = "MissionCardCell"
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    @IBOutlet weak var circleView: UIView!
+    @IBOutlet weak var indexLabel: UILabel!
+    @IBOutlet weak var mentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        circleView.backgroundColor = .none
+        circleView.setRounded(radius: nil)
+        circleView.setBorder(borderColor: .white, borderWidth: 1)
+        
+        indexLabel.font = UIFont.spoqaLight(size: 30)
+        indexLabel.textColor = .white
+        
+        mentLabel.font = UIFont.spoqaRegular(size: 20)
+        mentLabel.textColor = .white
+    }
+    
+    func setCard(_ missionCard: MissionCard) {
+        cardImage.image = missionCard.missionImage
+        indexLabel.text = "\(missionCard.missionIndex)"
+        mentLabel.text = missionCard.missonMent
+        
+    }
+    
+    
+    
 }
