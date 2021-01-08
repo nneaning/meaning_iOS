6bc6b5b01271d87d18a24c0d3f6dbf56c94e11b4
xwoud <xwoud@naver.com>
2021-01-09 04:17:43 +0900

    [upload] GroupFeedVC 생성
    body: 그룹 내부에 쓰이는 뷰컨트롤러 생성
    
    Related to: #73

---
+++ meaning/meaning/Screen/GroupFeed/ViewController/GroupFeedVC.swift
@@ -0,0 +1,29 @@
+//
+//  GroupFeedVC.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/09.
+//
+
+import UIKit
+
+class GroupFeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
