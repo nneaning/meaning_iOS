168cc54ff2dd01ca27a5acff35e24e8e84b216ef
xwoud <xwoud@naver.com>
2021-01-08 19:57:52 +0900

    [style] FeedDetailVC 기본 Style 설정
    body: 폰트 및 컬러 설정
    
    Related to: #65

--- meaning/meaning/Screen/GroupFeed/ViewController/FeedDetailVC.swift
+++ meaning/meaning/Screen/GroupFeed/ViewController/FeedDetailVC.swift
@@ -9,24 +9,40 @@ import UIKit
 
 class FeedDetailVC: UIViewController {
 
+    // MARK: IBOutlet
+    
     @IBOutlet weak var feedDetailTableView: UITableView!
+    @IBOutlet weak var feedNameLabel: UILabel!
+    
+    @IBOutlet weak var sloganView: UIView!
+    @IBOutlet weak var sloganLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setView()
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: Extension
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FeedDetailVC {
+    
+    func setView() {
+        
+        feedNameLabel.text = "마이피드"
+        feedNameLabel.font = UIFont.notoMedium(size: 17)
+        feedNameLabel.textColor = UIColor.gray1
+        
+        sloganView.backgroundColor = UIColor.meaningLightblue
+        
+        sloganLabel.font = UIFont.spoqaMedium(size: 14)
+        sloganLabel.textColor = .meaningNavy
+        sloganLabel.text = "오늘은 365일 중에 30번째 의미있는 아침입니다."
+        
+        feedDetailTableView.backgroundColor = .gray1
+        
     }
-    */
-
 }
