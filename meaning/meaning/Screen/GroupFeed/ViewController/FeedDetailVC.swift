e2c1a9b925bcb8cd34afdd096213ad73c45b099e
xwoud <xwoud@naver.com>
2021-01-08 15:17:13 +0900

    [upload] FeedDetailVC 생성

---
+++ meaning/meaning/Screen/GroupFeed/ViewController/FeedDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  FeedDetailVC.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/08.
+//
+
+import UIKit
+
+class FeedDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
