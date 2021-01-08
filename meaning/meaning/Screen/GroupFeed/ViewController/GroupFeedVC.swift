1a48859e406eed0fa7b4b4db9cb4159aa93d8d6d
xwoud <xwoud@naver.com>
2021-01-09 04:23:27 +0900

    [style] 더미데이터 삽입

--- meaning/meaning/Screen/GroupFeed/ViewController/GroupFeedVC.swift
+++ meaning/meaning/Screen/GroupFeed/ViewController/GroupFeedVC.swift
@@ -9,21 +9,42 @@ import UIKit
 
 class GroupFeedVC: UIViewController {
 
+    // MARK: Variable Part
+    
+    var feedList: [FeedImage] = []
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var groupFeedCollectionView: UICollectionView!
+    
+    // MARK: Life Cycle Part
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setData()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: Extension
+
+extension GroupFeedVC {
+    
+    // MARK: Function
+    
+    func setData() {
+        let data1 = FeedImage(feedImageName: "IMG_0668")
+        let data2 = FeedImage(feedImageName: "IMG_0668")
+        let data3 = FeedImage(feedImageName: "IMG_0668")
+        let data4 = FeedImage(feedImageName: "IMG_0668")
+        let data5 = FeedImage(feedImageName: "IMG_0668")
+        let data6 = FeedImage(feedImageName: "IMG_0668")
+        let data7 = FeedImage(feedImageName: "IMG_0668")
+        let data8 = FeedImage(feedImageName: "IMG_0668")
+        let data9 = FeedImage(feedImageName: "IMG_0668")
+        
+        feedList = [data1,data2,data3,data4,data5,data6,data7,data8,data9]
     }
-    */
-
 }
