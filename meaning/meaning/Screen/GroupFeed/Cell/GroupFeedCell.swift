9c77c1da260c67c925860a28339ff458457dd6be
xwoud <xwoud@naver.com>
2021-01-09 04:24:43 +0900

    [upload] GroupFeedCell 생성

---
+++ meaning/meaning/Screen/GroupFeed/Cell/GroupFeedCell.swift
@@ -0,0 +1,12 @@
+//
+//  GroupFeedCell.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/09.
+//
+
+import UIKit
+
+class GroupFeedCell: UICollectionViewCell {
+    static let identifier: String = "GroupFeedCell"
+}
