e4a7f814c4160577f695938350addd283341ea33
MinseungSeon <seonminseung@naver.com>
2021-01-08 15:57:01 +0900

    [fix] collection view의 scroll view inset 조정 dquote> body: dquote> 1. iphone SE 외의 큰 device 에서 collection view 아래 부분 safe area 가 비어있게 스크롤이 되는 문제 해결 2. UIScrollViewContentInsetAdjustment .never 로 설정함 3. 간단한 주석 추가함

--- meaning/meaning/Screen/MyPage/ViewController/MyPageFeedVC.swift
+++ meaning/meaning/Screen/MyPage/ViewController/MyPageFeedVC.swift
@@ -45,6 +45,10 @@ class MyPageFeedVC: UIViewController {
         feedCollectionView.register(FeedCollectionViewCell.nib(), forCellWithReuseIdentifier: FeedCollectionViewCell.identifier)
         feedCollectionView.delegate = self
         feedCollectionView.dataSource = self
+        
+        if #available(iOS 11.0, *) {
+            feedCollectionView.contentInsetAdjustmentBehavior = .never
+        }
     }
     
     override func viewWillLayoutSubviews() {
@@ -64,7 +68,7 @@ class MyPageFeedVC: UIViewController {
         self.headerLabel.font = UIFont.notoMedium(size: 17.0)
         self.headerLabel.text = "마이 피드"
         self.headerLabel.textColor = UIColor.gray1
- 
+        
         
         self.userNameInitial.text = initial
         self.userNameInitial.font = .spoqaMedium(size: 30)
@@ -101,13 +105,21 @@ class MyPageFeedVC: UIViewController {
         
     }
     
-    func setFeedImageData(){
-        feedImageList.append(contentsOf: [
-            FeedImage(feedImageName: "test_img")
-        ])
-    }
+    // MARK: setData
     
+    func setFeedImageData(){
+        // 임의로 40개 피드를 같은 사진으로 만들었음, 서버 연동을 하면서 수정하면 됨
+        for _ in 0...40 {
+            feedImageList.append(contentsOf: [
+                FeedImage(feedImageName: "test_img")
+            ])
+        }
+    }
 }
+
+// MARK: Extension
+
+// CollectionViewDelegate
 
 extension MyPageFeedVC: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -117,6 +129,8 @@ extension MyPageFeedVC: UICollectionViewDelegate {
     }
 }
 
+// CollectionViewDataSource
+
 extension MyPageFeedVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return feedImageList.count
@@ -124,18 +138,16 @@ extension MyPageFeedVC: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeedCollectionViewCell.identifier, for: indexPath) as! FeedCollectionViewCell
-        
         cell.configure(with: feedImageList[indexPath.row])
         
         return cell
     }
-    
-    
 }
 
+//FlowLayout
 
 extension MyPageFeedVC: UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = UIScreen.main.bounds.width/3
         return CGSize(width: width-0.5, height: width-0.5)
@@ -143,20 +155,17 @@ extension MyPageFeedVC: UICollectionViewDelegateFlowLayout {
     
     //전체 적으로 좌우상하 inset
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+        return UIEdgeInsets(top: 1, left: 0, bottom: 1, right: 0)
     }
-
+    
     //한 줄에서 아이템 좌우 사이 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0.5
     }
-
+    
     // 한 줄씩 사이의 거리
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
-    
-    
-    
 }
 
