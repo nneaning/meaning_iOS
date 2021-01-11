8fd31244da6c946d189df29e42203b6ba77547e7
MinseungSeon <seonminseung@naver.com>
2021-01-12 01:51:21 +0900

    [refac] 기상 인증 뷰 뷰디자인 수정에 따른 대응 처리 body: 1. 뷰에 들어가야하는 컴포넌트가 다소 많아서 SE 의 경우에는 '한 화면을 눌러서 수정하세요' 글씨를 넣지 않음 2. 개인적 욕심으로 textview 편집 시작하면 '흰 화면을 눌러서 수정하세요' 글씨 사라지게 처리해둠 3. iphone mini 에서도 아래가 조금 좁은 느낌이 들어서 이미지와 위 라벨(~꾸준히 기록해보아요) 사이 간격을 제플린 기준보다 5 만큼 줄였음

--- meaning/meaning/Screen/Mission/ViewController/PictureUploadVC.swift
+++ meaning/meaning/Screen/Mission/ViewController/PictureUploadVC.swift
@@ -10,22 +10,30 @@ import UIKit
 class PictureUploadVC: UIViewController {
     
     // MARK: Variable Part
-
+    
     var userNick: String = "이름"
     var nthMorning: Int = 22
     var uploadedImageData: UIImage? // 서버에다 줄 사진
     var timeToServer: String? // 서버에다 줄 사진 찍을 시간
-
+    
     // Mark: IBOutlet
-
+    
     @IBOutlet var headerView: UIView!
     @IBOutlet var headerLabel: UILabel!
     
     @IBOutlet var bodyUpperLabel: UILabel!
     @IBOutlet var uploadedImage: UIImageView!
     
-    @IBOutlet var bodyBottomLabel: UILabel!
+    @IBOutlet var bodyBottomView: UIView!
+    @IBOutlet var bodyTextView: UITextView!
+    
+    @IBOutlet var editGuideLabel: UILabel!
     @IBOutlet var uploadBtn: UIButton!
+    
+    @IBOutlet var upperLabelTopConstraint: NSLayoutConstraint!
+    @IBOutlet var upperLabelBottomConstraint: NSLayoutConstraint!
+    @IBOutlet var uploadBtnBottomConstraint: NSLayoutConstraint!
+    
     
     // MARK: IBAction
     
@@ -34,43 +42,71 @@ class PictureUploadVC: UIViewController {
     }
     
     // Mark: Life Cycle Part
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
-
+        
     }
     
     // MARK: Layout
     
     func setLayout() {
-        self.view.backgroundColor = UIColor.meaningLightblue
-        self.headerView.backgroundColor = UIColor.meaningLightblue
-        self.headerLabel.font = UIFont.notoMedium(size: 17.0)
-        self.headerLabel.text = "기상 인증"
-        self.headerLabel.textColor = UIColor.gray1
-
-        self.bodyUpperLabel.font = UIFont.notoRegular(size: 18.0)
-        self.bodyUpperLabel.textColor = UIColor.gray2
-        self.bodyUpperLabel.text = "\(userNick)님의 미라클 모닝을\n꾸준히 기록해 보아요"
-        self.bodyUpperLabel.lineSetting(kernValue: -0.72, lineSpacing: 5)
+        view.backgroundColor = UIColor.meaningLightblue
+        headerView.backgroundColor = UIColor.meaningLightblue
+        headerLabel.font = UIFont.notoMedium(size: 17.0)
+        headerLabel.text = "기상 인증"
+        headerLabel.textColor = UIColor.gray1
         
-        self.uploadedImage.setRounded(radius: 6)
-        self.uploadedImage.image = uploadedImageData
+        bodyUpperLabel.font = UIFont.notoRegular(size: 18.0)
+        bodyUpperLabel.textColor = UIColor.gray2
+        bodyUpperLabel.text = "\(userNick)님의 미라클 모닝을\n꾸준히 기록해 보아요"
+        bodyUpperLabel.lineSetting(kernValue: -0.72, lineSpacing: 5)
         
-        self.bodyBottomLabel.font = UIFont.notoRegular(size: 16.0)
-        self.bodyBottomLabel.textColor = UIColor.gray2
-        self.bodyBottomLabel.text = "\(userNick)님의 \(nthMorning)번째 의미있는 아침"
-        self.bodyBottomLabel.lineSetting(kernValue: -0.5)
+        uploadedImage.setRounded(radius: 6)
+        uploadedImage.image = uploadedImageData
+        bodyTextView.font = UIFont.notoRegular(size: 15.0)
+        bodyTextView.textColor = UIColor.gray2
+        bodyTextView.text = "\(userNick)님의 \(nthMorning)번째 의미있는 아침"
+        bodyTextView.lineSetting(kernValue: -0.6)
+        bodyTextView.delegate = self
         
+        bodyBottomView.setRounded(radius: 6)
+        bodyBottomView.backgroundColor = UIColor.meaningIvory
+        bodyTextView.backgroundColor = UIColor.white.withAlphaComponent(0)
         
-        self.uploadBtn.backgroundColor = UIColor.meaningNavy
-        self.uploadBtn.setTitleColor(.meaningWhite, for: .normal)
-        self.uploadBtn.titleLabel?.font = UIFont.spoqaMedium(size: 16)
-        self.uploadBtn.titleLabel?.lineSetting(kernValue: -0.64)
-        self.uploadBtn.setTitle("그룹에 사진올리기", for: .normal)
-        self.uploadBtn.setRounded(radius: 6)
+        editGuideLabel.text = "흰 화면을 눌러서 수정하세요"
+        editGuideLabel.font = .spoqaRegular(size: 14)
+        editGuideLabel.textColor = .gray3
+        editGuideLabel.lineSetting(kernValue: -0.56)
+        
+        uploadBtn.backgroundColor = UIColor.meaningNavy
+        uploadBtn.setTitleColor(.meaningWhite, for: .normal)
+        uploadBtn.titleLabel?.font = UIFont.spoqaMedium(size: 16)
+        uploadBtn.titleLabel?.lineSetting(kernValue: -0.64)
+        uploadBtn.setTitle("그룹에 사진올리기", for: .normal)
+        uploadBtn.setRounded(radius: 6)
+        
+        //SE의 경우에는 constraint 조정
+        if (view.frame.height <= 667) {
+            uploadBtnBottomConstraint.constant = 20
+            upperLabelTopConstraint.constant = 30
+            upperLabelBottomConstraint.constant = 30
+            //SE는 '흰 화면을 눌러서 수정하세요' 글자 들어갈 곳이 없어 숨김처리 함
+            editGuideLabel.isHidden = true
+        }
     }
-  
+    
+}
 
+// MARK: Extension
+
+extension PictureUploadVC: UITextViewDelegate {
+    
+    // MARK: Function
+    
+    //편집 시작하면 가이드 글자가 사라지도록 해주었음
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        editGuideLabel.isHidden = true
+    }
 }
