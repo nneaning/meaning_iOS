70441d71ed7d034f54bc277e0ae2cce3a49d8588
xwoud <51286963+xwoud@users.noreply.github.com>
2021-01-13 15:36:41 +0900

    [network] 캘린더 서버 연결하기 (#164)
    * [docs] podfile
    
    * [upload] CalendarData 구조체 생성
    
    body: 이중 구조체 형태
    
    Related to: #162
    
    * [upload] 캘린더 api 추가하기
    
    * [network] 캘린더 서버 연동 성공
    
    body: 데이터 받아와서 뷰에 뿌려주기 성공
    
    Related to: #162

---
+++ meaning/meaning/Global/Model/CalendarData.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarData.swift
+//  meaning
+//
+//  Created by 김민희 on 2021/01/13.
+//
+
+import Foundation
+
+struct CalendarData: Codable {
+    let successDays: Int
+    let calendar: [CalendarDay]
+}
+
+// MARK: - CalendarDay
+struct CalendarDay: Codable {
+    let dateTime: String
+    let status: Int
+}
