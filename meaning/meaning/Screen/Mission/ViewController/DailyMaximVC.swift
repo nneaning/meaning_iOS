b42a3c860ffafa8635567e9dfda8b84656166aae
MinseungSeon <seonminseung@naver.com>
2021-01-13 18:47:27 +0900

    [network] Mission-하루다짐 뷰 서버 연결 완료 body: GET 메소드로 토큰 넘겨서 연결 완료함.

--- meaning/meaning/Screen/Mission/ViewController/DailyMaximVC.swift
+++ meaning/meaning/Screen/Mission/ViewController/DailyMaximVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class DailyMaximVC: UIViewController {
     
+    // Mark: Variables
+    
+    var daypromiseData: DaypromiseData?
+    
     // Mark: IBOutlet
     
     @IBOutlet var headerView: UIView!
@@ -34,6 +38,9 @@ class DailyMaximVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
+//        daypromise(token: UserDefaults.standard.string(forKey: "accesstoken"))
+        //test 용 토큰 넣은 서버 통신 line
+        daypromise(token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsIm5hbWUiOiJtaW5zZXVuZyIsImlhdCI6MTYxMDUyODQ0MywiZXhwIjoxNjEyMzQyODQzLCJpc3MiOiJTZXJ2ZXJCYWQifQ.Hm9BJcZl_ukgc-ce3Fdz2EsK8ABHozGgGieZhFbi4NM")
     }
     
     // MARK: Layout
@@ -44,7 +51,7 @@ class DailyMaximVC: UIViewController {
         self.headerLabel.font = UIFont.notoMedium(size: 17.0)
         self.headerLabel.text = "하루 다짐"
         self.headerLabel.textColor = UIColor.gray1
-
+        
         self.bodyUpperLabel.font = UIFont.notoRegular(size: 18.0)
         self.bodyUpperLabel.textColor = UIColor.gray2
         self.bodyUpperLabel.text = "하루의 시작부터 글귀를 읽으며\n성장과 발전의 의지를 다져요"
@@ -53,11 +60,11 @@ class DailyMaximVC: UIViewController {
         self.bodyViewLabel.text = "평범한 사람들은 가능한 것만 믿는다.\n특별한 사람들은 불가능한 것을 그려본다.\n그리고 그것을 가능한 것으로 보기 시작한다."
         self.bodyViewLabel.font = UIFont.nbRegular(size: 18)
         self.bodyViewLabel.lineSetting(kernValue: -0.5, lineSpacing: 10)
-
+        
         self.bodyBottomLabel.font = UIFont.notoRegular(size: 15.0)
         self.bodyBottomLabel.textColor = UIColor.gray3
         self.bodyBottomLabel.text = "3번 반복해서 읽어주세요."
-
+        
         self.ReadComplete.backgroundColor = UIColor.meaningNavy
         self.ReadComplete.setTitleColor(UIColor.meaningWhite, for: .normal)
         self.ReadComplete.titleLabel?.font=UIFont.spoqaMedium(size: 16)
@@ -65,5 +72,40 @@ class DailyMaximVC: UIViewController {
         self.ReadComplete.setRounded(radius: 6)
     }
     
+    // MARK: Server function
+    
+    func daypromise(token: String){
+        APIService.shared.daypromise(token) {
+            result in
+            switch result {
+            
+            case .success(let data):
+                self.daypromiseData = data
+                if let daypromiseData = self.daypromiseData {
+                    self.bodyViewLabel.text = daypromiseData.contents
+                }
+                
+            case .failure(let error):
+                if (error == 401) {
+                    self.showToast(message: "다시 로그인을 해주세요!", font: UIFont.spoqaRegular(size: 16))
+                } else {
+                    self.showToast(message: "네트워크 연결을 확인해주세요.", font: UIFont.spoqaRegular(size: 16))
+                }
+            }
+        }
+    }
 }
 
+
+// MARK: APIService
+
+extension APIService {
+    
+    func daypromise(_ token: String, completion: @escaping (NetworkResult<DaypromiseData>)->(Void)) {
+        
+        let target: APITarget = .daypromise(token: token)
+        judgeObject(target, completion: completion)
+        
+    }
+    
+}
