747c4fda77214d45314a85f0139fb67b975b0e52
xwoud <xwoud@naver.com>
2021-01-13 17:36:36 +0900

    [fix] 스플래쉬 뷰 분기처리
    body: 로그인 후 온보딩을 하지 않았을 경우 다시 앱을 종료 후 들어왓을 때 로그인으로 가게 시킴

--- meaning/meaning/Screen/Splash/ViewController/LaunchScreenVC.swift
+++ meaning/meaning/Screen/Splash/ViewController/LaunchScreenVC.swift
@@ -43,8 +43,18 @@ class LaunchScreenVC: UIViewController {
                            loopMode: LottieLoopMode.playOnce,
                            completion: { (finished) in
                             if finished {
-                                
-                                self.checkToken()
+                                if (UserDefaults.standard.string(forKey: "userNick") != nil) {
+                                    // 온보딩 설정이 존재하면?
+                                    self.checkToken()
+                                } else {
+                                    // 로그인 뷰로 이동
+                                    let storyBoard: UIStoryboard = UIStoryboard(name: "Login", bundle: nil)
+                                    let loginViewController = storyBoard.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+                                    loginViewController.modalTransitionStyle = .crossDissolve
+                                    loginViewController.modalPresentationStyle = .fullScreen
+                                    self.present(loginViewController, animated: true, completion: nil)
+                                    
+                                }
                                 
                             } else {
                                 print("Animation cancelled")
