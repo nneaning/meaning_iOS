dc6d89fd60b006818e8f64a430c52994138c9ea4
xwoud <51286963+xwoud@users.noreply.github.com>
2021-01-14 02:32:38 +0900

    [feat] ~번째 기준 수정하기 (#183)
    * [docs] podfile
    
    * [feat] ~번째 기준 수정
    
    body: 지금까지 올린 글 + 1
    
    * [feat] ~번째 수정
    
    body: 마이페이지에도 같은 내용이 있어서 수정

--- meaning/meaning/Screen/MyPage/ViewController/MyPageFeedVC.swift
+++ meaning/meaning/Screen/MyPage/ViewController/MyPageFeedVC.swift
@@ -99,7 +99,7 @@ class MyPageFeedVC: UIViewController {
         self.headerLabel.textColor = UIColor.gray1
         
         self.userNameInitial.text = initial
-        self.userNameInitial.font = .spoqaMedium(size: 30)
+        self.userNameInitial.font = .spoqaMedium(size: 20)
         self.userNameInitial.textAlignment = .center
         
         self.userNameLabel.text = userNick
@@ -155,7 +155,7 @@ class MyPageFeedVC: UIViewController {
                 case .success(let data):
                     self.mypageData = data
                     if let mypage = self.mypageData?.getMyPage {
-                        self.feedHeaderLabel.text = "오늘은 365일 중에 \(mypage.count)번째 의미있는 아침입니다."
+                        self.feedHeaderLabel.text = "오늘은 365일 중에 \(mypage.count + 1)번째 의미있는 아침입니다."
                     }
                     self.feedCollectionView.reloadData()
 
